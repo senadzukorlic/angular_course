d29637fd0e0a5ef5837fa3209517ca84fbbd1a97
Senad Zukorlic <zukorlic.senad01@gmail.com>
2025-01-28 09:12:11 +0100

    Managing & Creating Components with the Angular CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header.copomnent';
+import { HeaderComponent } from './header/header.copomnent';
 
 @Component({
   selector: 'app-root',
