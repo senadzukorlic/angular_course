c9cc7c40c2ffb62e58c7cde56c99c60aa2198833
Senad Zukorlic <zukorlic.senad01@gmail.com>
2025-01-31 10:14:46 +0100

    wrote simple explains for lessons I learned

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,14 +11,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 export class UserComponent {
   selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar); //koristi se samo da izracuna vrednost signala sa nekom drugom vrednosti.
 
   // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar;
+  //   return 'assets/users/' + this.selectedUser.avatar; //geter se koristi da omoguci pristup vrednostima objekta u kontrolisanoj formi.
   // }
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]); //set postavlja/menja vrednost koja je postavljena u signalu
   }
 }
