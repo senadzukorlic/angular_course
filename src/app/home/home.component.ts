9f178de3c0dc4d9960014609c17ff52c0570f4c8
Senad Zukorlic <zukorlic.senad01@gmail.com>
2025-03-07 22:49:11 +0100

    Zamenjen stari kod sa novim projektom

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { GreetingComponent } from '../components/greeting/greeting.component';
+import { sign } from 'crypto';
+
+@Component({
+  selector: 'app-home',
+  imports: [GreetingComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  homeMessage = signal('Hello, World!');
+}
